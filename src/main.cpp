949487d3f46eba494ae72dca8b248305fe4e56c7
GBY <kwancr92@gmail.com>
2019-04-10 14:23:28 +0800

    op Bresenham_line_algorithm(Remove floating point arithmetic)

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
-int main(int argc, char **argv)
-{
-    TGAImage image(500, 500, TGAImage::RGB);
-    image.line(100, 100, 400, 400, red);
-    // image.flip_horizontally();
-    // image.flip_vertically();
-    image.write_tga_file("output.tga");
-    return 0;
+int main(int argc, char **argv) {
+  TGAImage image(500, 500, TGAImage::RGB);
+  image.line(100, 100, 400, 400, red);
+  // image.flip_horizontally();
+  // image.flip_vertically();
+  image.write_tga_file("output.tga");
+  return 0;
 }
