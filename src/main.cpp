54b67b2d534c326ee37cc64bed6a0d8f7149af2d
GBY <kwancr92@gmail.com>
2019-04-12 10:06:36 +0800

    emm

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "tga.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor red = TGAColor(255, 0, 0, 0);
 
 void aliased_line();
 
@@ -16,13 +16,13 @@ int main(int argc, char **argv) {
 void aliased_line() {
   TGAImage image(100, 100, TGAImage::RGB);
   image.line(50, 10, 60, 90, red);
-  image.flip_horizontally();
+  // image.flip_horizontally();
   image.write_tga_file("output_aliased.tga");
 }
 
 void anti_aliased_line() {
   TGAImage image(500, 500, TGAImage::RGBA);
-  image.anti_aliasing_line(50.3, 10, 60, 90.1, red);
-  image.flip_horizontally();
+  image.anti_aliasing_line(50.0, 10.0, 60.0, 90.0, red);
+  // image.flip_horizontally();
   image.write_tga_file("output_anti_aliased.tga");
 }
